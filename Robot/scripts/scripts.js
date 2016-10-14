5c3e960eeb781ea63fc7ccde400c17c0cb3de061
Jeanosis <plaits123@gmail.com>
2016-10-15 00:15:09 +0300

    Script added.

---
+++ Robot/scripts/scripts.js
@@ -0,0 +1,64 @@
+addEventListener("mousemove", trackMouse, false);
+
+function trackMouse(event)
+{
+    var leftIris = document.getElementById('left-iris');
+    var leftEye = leftIris.parentElement;
+    var width = (leftEye.parentElement.offsetWidth - leftEye.offsetWidth) / 2;
+    
+    moveIris(document.getElementById('left-iris'), width);
+    moveIris(document.getElementById('right-iris'), width);
+    /*var iris = document.getElementById('left-iris');var eyeArea = iris.parentElement.getBoundingClientRect();
+    var irisArea = iris.getBoundingClientRect();
+
+    var areaMultiplier = 5;
+    var width = areaMultiplier * eyeArea.width, height = areaMultiplier * eyeArea.height;
+    var center = {
+        x: eyeArea.left + (eyeArea.width / 2),
+        y: eyeArea.top + (eyeArea.height / 2)
+    };
+
+    var multiplier = (width + width) / (eyeArea.width - irisArea.width);
+
+    if (event.pageX <= center.x - width)
+        iris.style.left = "0px";
+    else if (event.pageX >= center.x + width)
+        iris.style.left = (eyeArea.width - irisArea.width) + "px";
+    else
+        iris.style.left = Math.floor((event.pageX - (center.x - width)) / multiplier) + "px";
+
+    if (event.pageY <= center.y - height)
+        iris.style.top = "0px";
+    else if (event.pageY >= center.y + height)
+        iris.style.top = (eyeArea.height - irisArea.height) + "px";
+    else
+        iris.style.top = Math.floor((event.pageY - (center.y - height)) / multiplier) + "px";*/
+}
+
+function moveIris(iris, width)//areaMultiplier)
+{
+    var eyeArea = iris.parentElement.getBoundingClientRect();
+    var irisArea = iris.getBoundingClientRect();
+
+    var /*width = areaMultiplier * eyeArea.width,*/ height = (width * eyeArea.height) / eyeArea.width;
+    var center = {
+        x: eyeArea.left + (eyeArea.width / 2),
+        y: eyeArea.top + (eyeArea.height / 2)
+    };
+    
+    var multiplier = (width + width) / (eyeArea.width - irisArea.width);
+
+    if (event.pageX <= center.x - width)
+        iris.style.left = "0px";
+    else if (event.pageX >= center.x + width)
+        iris.style.left = (eyeArea.width - irisArea.width) + "px";
+    else
+        iris.style.left = Math.floor((event.pageX - (center.x - width)) / multiplier) + "px";
+
+    if (event.pageY <= center.y - height)
+        iris.style.top = "0px";
+    else if (event.pageY >= center.y + height)
+        iris.style.top = (eyeArea.height - irisArea.height) + "px";
+    else
+        iris.style.top = Math.floor((event.pageY - (center.y - height)) / multiplier) + "px";
+}
